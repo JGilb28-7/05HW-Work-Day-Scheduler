b6026099d367b4374e9dfd13f6119075c1f05944
JGilbe28-7 <jonathan.gilbert@mac.com>
2020-10-23 17:54:46 -0400

    commit HTML - update .js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 05HW-Work-Day-Scheduler
 Homework 05 Third-Party APIs: Work Day Scheduler
+used the started code provided for the base HTML and .CSS back worked the .css to add the elements to the HTML see Weblinks for addtional support 
+
+# WebLinks
+https://www.w3schools.com/tags/tag_textarea.asp
+https://www.w3schools.com/icons/tryit.asp?filename=tryicons_fa-save
---
+++ assets/script.js
@@ -0,0 +1,21 @@
+//create var/let
+// function
+//need to loop over the time 
+//moment
+// local storage
+// color change
+/*Hour 1.0.0+
+
+moment().hour(Number);
+moment().hour(); // Number
+moment().hours(Number);
+moment().hours(); // Number
+Gets or sets the hour.
+
+Accepts numbers from 0 to 23. If the range is exceeded, it will bubble up to the day.*/
+
+
+
+moment().date(Number);
+moment().date(); // Number
+console.log(moment().date());
