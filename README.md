c473191e129a3c7a3899498389d4bca3e08127bf
JGilbe28-7 <jonathan.gilbert@mac.com>
2020-10-24 17:12:14 -0400

    cleaning up .js time display complete

--- README.md
+++ README.md
@@ -4,4 +4,14 @@ used the started code provided for the base HTML and .CSS back worked the .css t
 
 # WebLinks
 https://www.w3schools.com/tags/tag_textarea.asp
+
 https://www.w3schools.com/icons/tryit.asp?filename=tryicons_fa-save
+
+https://www.sitepoint.com/managing-dates-times-using-moment-js/
+moment().format('YYYY-MM-DD');
+
+https://www.webfx.com/blog/web-design/javascript-dates-moment-js/
+use Moment.js to display current date in a div (#displayMoment)
+var NowMoment = moment();
+var eDisplayMoment = document.getElementById('displayMoment');
+eDisplayMoment.innerHTML = NowMoment.format('YYYY-M-D');
--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,26 @@
-//need to create document link
-//create var/let
+// display the date in the - used several options - needed to remove the text I added
+let currentDay = $("#currentDay");
+let m = moment();
+currentDay.append(m.format(" dddd[,] LL"));
+
+////create var/let - var stringVariable = "string"; - var numberVariable = 12;
+let container = $(".container")
+let dataObj = JSON.parse(localStorage.getItem("allData")) || {};
+let hours = ["8:00am", "9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"];
+let values = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+
 // function
+
 //need to loop over the time 
+//(let=0; i=0 < Array.length; i++)
+
 //moment
+
 // local storage
     // need to store the data for each hour 
+    //localStorage.setItem("the_key", "a value");
+    //var valueFromLocalStorage = localStorage.getItem("the_key");
+
 // color change
 /*Hour 1.0.0+
 
@@ -14,10 +30,3 @@ moment().hours(Number);
 moment().hours(); // Number
 Gets or sets the hour.
 
-Accepts numbers from 0 to 23. If the range is exceeded, it will bubble up to the day.*/
-
-
-
-moment().date(Number);
-moment().date(); // Number
-console.log(moment().date());
